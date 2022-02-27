bb62a6ef13b778b5fd504408ec67f5459950fb1e
outersky <outersky@gmail.com>
2022-02-27 18:00:12 +0800

    feat: implement query core classes

---
+++ lib/src/query.dart
@@ -0,0 +1,102 @@
+abstract class Column<T, R> {
+  final List<ColumnCondition> conditions = [];
+
+  R _addCondition(ColumnConditionOper oper, dynamic value) {
+    conditions.add(ColumnCondition(oper, value));
+    return this as R;
+  }
+
+  R eq(T value) => _addCondition(ColumnConditionOper.EQ, value);
+
+  R IN(List<T> value) => _addCondition(ColumnConditionOper.IN, value);
+
+  R notIn(List<T> value) => _addCondition(ColumnConditionOper.NOT_IN, value);
+}
+
+mixin NullCondition<T, R> on Column<T, R> {
+  R isNull() => _addCondition(ColumnConditionOper.IS_NULL, null);
+
+  R isNotNull() => _addCondition(ColumnConditionOper.IS_NOT_NULL, null);
+}
+
+mixin ComparableCondition<T, R> on Column<T, R> {
+  R gt(T value) => _addCondition(ColumnConditionOper.GT, value);
+
+  R ge(T value) => _addCondition(ColumnConditionOper.GE, value);
+
+  R lt(T value) => _addCondition(ColumnConditionOper.LT, value);
+
+  R le(T value) => _addCondition(ColumnConditionOper.LE, value);
+
+  R between(T beginValue, T endValue) =>
+      _addCondition(ColumnConditionOper.BETWEEN, [beginValue, endValue]);
+
+  R notBetween(T beginValue, T endValue) =>
+      _addCondition(ColumnConditionOper.NOT_BETWEEN, [beginValue, endValue]);
+
+/* 
+  R operator >(T value) => gt(value);
+  R operator <(T value) => lt(value);
+  R operator >=(T value) => ge(value);
+  R operator <=(T value) => le(value);
+ */
+}
+
+class ColumnCondition {
+  final ColumnConditionOper oper;
+  final dynamic value;
+
+  ColumnCondition(this.oper, this.value);
+
+  @override
+  String toString() => '(${oper.name}: ${value})';
+}
+
+class NumberColumn<T, R> extends Column<T, R> with ComparableCondition<T, R> {}
+
+class IntColumn extends NumberColumn<int, IntColumn> {}
+
+class DoubleColumn extends NumberColumn<double, DoubleColumn> {}
+
+class StringColumn extends Column<String, StringColumn>
+    with
+        ComparableCondition<String, StringColumn>,
+        NullCondition<String, StringColumn> {
+  StringColumn like(String pattern) =>
+      _addCondition(ColumnConditionOper.LIKE, pattern);
+
+  StringColumn startsWith(String prefix) =>
+      _addCondition(ColumnConditionOper.LIKE, '%$prefix');
+
+  StringColumn endsWith(String prefix) =>
+      _addCondition(ColumnConditionOper.LIKE, '$prefix%');
+
+  StringColumn contains(String subString) =>
+      _addCondition(ColumnConditionOper.LIKE, '%$subString%');
+}
+
+class BoolColumn extends Column<bool, BoolColumn> {
+  BoolColumn isTrue() => _addCondition(ColumnConditionOper.EQ, true);
+
+  BoolColumn isFalse() => _addCondition(ColumnConditionOper.EQ, false);
+}
+
+class DateTimeColumn extends Column<DateTime, DateTimeColumn>
+    with
+        ComparableCondition<DateTime, DateTimeColumn>,
+        NullCondition<DateTime, DateTimeColumn> {}
+
+enum ColumnConditionOper {
+  EQ,
+  GT,
+  LT,
+  GE,
+  LE,
+  BETWEEN,
+  NOT_BETWEEN,
+  LIKE,
+  IN,
+  NOT_IN,
+  IS_NULL,
+  IS_NOT_NULL
+}
--- test/query_test.dart
+++ test/query_test.dart
@@ -1,53 +1,25 @@
-import 'package:needle_orm/needle_orm.dart';
+import 'package:needle_orm/src/query.dart';
 import 'package:test/test.dart';
 
-typedef C = SqlCondition;
-
 void main() {
-  test('condition test', () async {
-    SqlAnd c = SqlAnd();
-    c +
-        C("t0.deleted = 0") +
-        C("t0.name = @name", {'name': 'admin'}) +
-        SqlOr([
-          C("t1.age > @age", {"age": 10}),
-          C("t1.gendar = @gendar", {'gendar': 'F'}),
-        ]);
+  test('column condition test', () {
+    var userDef = UserDef();
+    userDef
+      ..age.gt(10).lt(20)
+      ..name.startsWith('admin')
+      ..active.isTrue()
+      ..birthday.between(
+          DateTime.now().subtract(Duration(days: 365 * 10)), DateTime.now());
 
-    expect(c.toSql(wrap: false),
-        "t0.deleted = 0 AND t0.name = @name AND (t1.age > @age OR t1.gendar = @gendar)");
-    expect(c.params.length, 3);
+    userDef.columns.map((e) => e.conditions).forEach(print);
   });
+}
 
-  test('join test', () {
-    SqlJoin j = SqlJoin('user', 'u', SqlJoinType.LEFT);
-    j.conditions + C("u.deleted = 0") + C("t0.id = u.id");
+class UserDef {
+  IntColumn age = IntColumn();
+  StringColumn name = StringColumn();
+  DateTimeColumn birthday = DateTimeColumn();
+  BoolColumn active = BoolColumn();
 
-    expect(j.toSql(), 'LEFT join user u on u.deleted = 0 AND t0.id = u.id');
-  });
-
-  test('complete query test', () {
-    SqlQuery q = SqlQuery('user', 'u', distinct: true);
-    q.columns.addAll(['u.id', 'u.name', 'u.email', 'u.deleted']);
-    q.joins.addAll([
-      SqlJoin('user_role', 'ur', SqlJoinType.LEFT).apply((j) {
-        j.conditions..append(C("ur.user_id = u.id"));
-      }),
-      SqlJoin('role', 'r', SqlJoinType.LEFT).apply((j) {
-        j.conditions
-          ..append(C("r.deleted = 0"))
-          ..append(C("r.name like @role_name", {'role_name': '%member'}))
-          ..append(C("r.id = ur.role_id"));
-      })
-    ]);
-    q.conditions
-      ..append(C("u.deleted = 0"))
-      ..append(C("u.email like @email", {'email': '%@gmail.com'}));
-    expect(q.toSql(),
-        'select distinct u.id, u.name, u.email, u.deleted from user u LEFT join user_role ur on ur.user_id = u.id LEFT join role r on r.deleted = 0 AND r.name like @role_name AND r.id = ur.role_id where u.deleted = 0 AND u.email like @email');
-    expect(q.joins.params['role_name'], '%member');
-    expect(q.conditions.params['email'], '%@gmail.com');
-
-    expect({...q.joins.params, ...q.conditions.params}.length, 2);
-  });
+  List<Column> get columns => [age, name, birthday, active];
 }
